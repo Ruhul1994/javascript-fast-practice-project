a176c9e17a38c8d8795a3b5c59198d4c8b474304
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-08-24 01:27:33 +0000

    some type and function test

---
+++ .devcontainer/basics/main.js
@@ -0,0 +1,13 @@
+// console.log('Hello world');
+
+//  const NumberFast = true;
+//  console.log(typeof (NumberFast));
+let userName ='john';
+
+function showMessage(){
+// let userName = "Bob";
+let message = 'Hello,'+ userName;
+// alert(message);
+console.log(message);                   
+}
+showMessage();
