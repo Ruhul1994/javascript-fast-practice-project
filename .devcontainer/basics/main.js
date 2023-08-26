4a35000be440d0e803d4fce7865acf21d7da89b0
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-08-26 17:13:51 +0000

    mac number added

--- .devcontainer/basics/main.js
+++ .devcontainer/basics/main.js
@@ -16,3 +16,13 @@ let myNumber = Math.random();
 return myNumber;
 }
 randomNumber();
+/*192.168.31.140
+5A:2F:39:22:75:15 	17min48s 	
+realme-C11-2021 	192.168.31.6
+2A:68:32:C0:1B:83 	1min49s 	
+DESKTOP-3K3KNTG 	192.168.31.14
+C0:B6:F9:A0:4A:51 	3min10s 	
+Galaxy-A01 	192.168.31.204
+26:23:63:08:02:5F 	3min54s 	
+Ruhul 	192.168.31.144
+48:A4:72:69:33:59*/
