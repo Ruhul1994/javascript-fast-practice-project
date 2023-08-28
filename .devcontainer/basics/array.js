305ac374de9dbd3e3460534b7fcd494a2d7ceb55
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-08-28 20:29:35 +0000

    added flat method.

--- .devcontainer/basics/array.js
+++ .devcontainer/basics/array.js
@@ -1,5 +1,8 @@
-let myArray = [1,2,3,4];
-let myNextArray = [8,9,12,13];
-// let myNewArray = myArray.concat(myNextArray);
-let myNewArray = [...myArray,...myNextArray];
-console.log(`my new array = ${myNewArray}`);
+// let myArray = [1,2,3,4];
+// let myNextArray = [8,9,12,13];
+// // let myNewArray = myArray.concat(myNextArray);
+// let myNewArray = [...myArray,...myNextArray];
+// console.log(`my new array = ${myNewArray}`);
+const anatherArray = [1,2,3,4,[5,6,7],[8,9,[10,11,12],13,14]]
+const myRealArray = anatherArray.flat(Infinity);
+console.log(myRealArray);
