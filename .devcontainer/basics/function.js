cd480c6e01dda4c67c305f2c9311c699328a000a
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-08-30 06:39:49 +0000

    anonimas fonction added

---
+++ .devcontainer/basics/function.js
@@ -0,0 +1,10 @@
+function toAdded(x,y){
+    return x + y;
+}
+let result = toAdded(12,10);
+console.log(result);
+let add = function(a,b){
+    return a * b;
+} 
+let myresult = add(4,5);
+console.log(myresult);
