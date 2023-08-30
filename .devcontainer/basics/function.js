ec80bc94bfa175380275d310d371bec37fd5dbe8
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-08-30 08:24:37 +0000

    Arro function added

--- .devcontainer/basics/function.js
+++ .devcontainer/basics/function.js
@@ -1,10 +1,16 @@
-function toAdded(x,y){
-    return x + y;
-}
+// function toAdded(x,y){
+//     return x + y;
+// }
+const toAdded = (x,y) => x + y;
+
 let result = toAdded(12,10);
+
 console.log(result);
-let add = function(a,b){
-    return a * b;
-} 
+
+// let add = function(a,b){
+//     return a * b;
+// } 
+const add = (a,b) => a * b;
 let myresult = add(4,5);
 console.log(myresult);
+//node .devcontainer/basics/function.js
