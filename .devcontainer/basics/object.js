7d1eccaf014384d6b591848e56cae56985f921a5
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-09-03 03:30:02 +0000

    a objecte added

--- .devcontainer/basics/object.js
+++ .devcontainer/basics/object.js
@@ -1,6 +1,19 @@
-myObject = {
+const myObject = {
     name:'Ruhul',
     gmail:'ruhulkuddus@gmail.com',
     number:'01722126426',
 }
-console.log(myObject.number);
+// console.log(myObject.number);
+
+const object1={
+a:'1',
+b:'2',
+c:'3'
+}
+const object2 = {
+    a:'4',
+    b:'5',
+    c:'6'
+}
+object3 = {...object2, ...object1 };
+console.log(object3);
