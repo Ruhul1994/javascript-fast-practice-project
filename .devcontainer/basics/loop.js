cfc96511abc647858b80d77c6afa0d44513dc138
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-09-08 05:06:54 +0000

    added while and do while loop.

---
+++ .devcontainer/basics/loop.js
@@ -0,0 +1,10 @@
+
+// while (true) {
+//   let num = Math.ceil(Math.random() * 100);
+//   console.log("Ruhul", num);
+//   if (num == 99) break;
+// }
+
+// do{
+//   console.log('Hello world this is the  last print')
+// } while(false)
