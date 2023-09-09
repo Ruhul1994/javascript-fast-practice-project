6881c2431d871ec183c2b5a82729c46eb59c5f7d
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-09-09 07:52:12 +0000

    added a spread operator

--- .devcontainer/basics/object.js
+++ .devcontainer/basics/object.js
@@ -1,25 +1,35 @@
-const myObject = {
-    name:'Ruhul',
-    gmail:'ruhulkuddus@gmail.com',
-    number:'01722126426',
-}
-// console.log(myObject.number);
+// const myObject = {
+//     name:'Ruhul',
+//     gmail:'ruhulkuddus@gmail.com',
+//     number:'01722126426',
+// }
+// // console.log(myObject.number);
 
-const object1={
-a:'1',
-b:'2',
-c:'3'
+// const object1={
+// a:'1',
+// b:'2',
+// c:'3'
+// }
+// const object2 = {
+//     a:'4',
+//     b:'5',
+//     c:'6'
+// }
+// object3 = {...object2, ...object1 };
+// console.log(object3);
+// const teacher = {
+//     "name":"Tomas",
+//     "subject":"Math",
+//     "class":"ten",
+//     "school":"secendary"
+// }
+const parson = {
+    Name:"Ruhul",
+    Age:23
 }
-const object2 = {
-    a:'4',
-    b:'5',
-    c:'6'
+const carecter ={
+    hoby: "web design",
+    city:"khulna"
 }
-object3 = {...object2, ...object1 };
-console.log(object3);
-const teacher = {
-    "name":"Tomas",
-    "subject":"Math",
-    "class":"ten",
-    "school":"secendary"
-}
+const allBio = { ...parson, ...carecter };
+console.log(allBio);
