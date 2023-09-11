f88800456c38a8bc0c19ba3125604f8fe2ebaac0
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-09-11 08:27:57 +0000

    a new function added

--- .devcontainer/basics/function.js
+++ .devcontainer/basics/function.js
@@ -1,23 +1,31 @@
 // function toAdded(x,y){
 //     return x + y;
 // }
-const toAdded = (x,y) => x + y;
+// const toAdded = (x,y) => x + y;
 
-let result = toAdded(12,10);
+// let result = toAdded(12,10);
 
-console.log(result);
+// console.log(result);
 
-// let add = function(a,b){
-//     return a * b;
-// } 
-const add = (a,b) => a * b;
-let myresult = add(4,5);
-console.log(myresult);
-//node .devcontainer/basics/function.js
-let a = Symbol(122);
-let b =122;
-if(a==b){
-    console.log(true);
-}else{
-    console.log(false);
+// // let add = function(a,b){
+// //     return a * b;
+// // } 
+// const add = (a,b) => a * b;
+// let myresult = add(4,5);
+// console.log(myresult);
+// //node .devcontainer/basics/function.js
+// let a = Symbol(122);
+// let b =122;
+// if(a==b){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+function myName(name = 'Ruhul'){
+   if (!name) {
+       console.log('please inter the name');
+       return
+    }
+    return `${name} is a good programar in the world.`
 }
+console.log(myName());
