470145a4d71846ae1d79e263db75b20f2889c4cd
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-09-12 14:13:31 +0000

    a forEach method is added

--- .devcontainer/basics/array.js
+++ .devcontainer/basics/array.js
@@ -3,19 +3,29 @@
 // // let myNewArray = myArray.concat(myNextArray);
 // let myNewArray = [...myArray,...myNextArray];
 // console.log(`my new array = ${myNewArray}`);
-const anatherArray = [1,2,3,4,[5,6,7],[8,9,[10,11,12],13,14]];
+// const anatherArray = [1,2,3,4,[5,6,7],[8,9,[10,11,12],13,14]];
 
- console.log(Array.isArray('ruhul'));
+//  console.log(Array.isArray('ruhul'));
  
-const myRealArray = anatherArray.flat(Infinity);
-// node .devcontainer/basics/array.js
+// const myRealArray = anatherArray.flat(Infinity);
+// // node .devcontainer/basics/array.js
 
-console.log(myRealArray);
+// console.log(myRealArray);
 
-console.log(Array.from("Ruhul"));
-let score1 = 100;
-let score2 = 200;
-let score3 = 300;
-let score4 =500;
+// console.log(Array.from("Ruhul"));
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// let score4 =500;
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
+
+const myArray = [2,3,4,5,6,7,8,9];
+let sum = 0;
+myArray.forEach((v,i)=>{
+if(i <= 3){
+ sum += v;
+}
+
+})
+console.log(sum);
