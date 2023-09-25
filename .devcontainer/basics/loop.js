aa41ea500945e68f6214121c38f9923d7a3b2bde
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-09-25 04:01:28 +0000

    a nested loop added.

--- .devcontainer/basics/loop.js
+++ .devcontainer/basics/loop.js
@@ -9,5 +9,12 @@
 //   console.log('Hello world this is the  last print')
 // } while(false)
 
-const rpt = `I want to say   ${' I love Allah +' . repeat(5) } waleys `;
-console.log(rpt);
+// const rpt = `I want to say   ${' I love Allah +' . repeat(5) } waleys `;
+// console.log(rpt);
+const elements = ['A', 'B', 'C', 'D'];
+
+for (let i = 0; i < elements.length; i++) {
+  for (let j = i + 1; j < elements.length; j++) {
+    console.log(elements[i] + elements[j]);
+  }
+}
