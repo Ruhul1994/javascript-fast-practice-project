b8a6fc8b103d8e07d9f10925aaceb7d6acc5be43
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2023-10-05 05:44:49 +0000

    use a for of loop in set

--- .devcontainer/basics/set.js
+++ .devcontainer/basics/set.js
@@ -1,7 +1,11 @@
-let frutes = new Set(['mango','Apple','banana', 'orenge','orenge']);
+let frutes = new Set(['mango','Apple','banana', 'orenge','orenge','nut', 'jacfrute', 'chari','pinapple']);
 console.log(frutes);
 let newFrutes = "";
-frutes.forEach((frute) => {
-    newFrutes += frute + ' ';
-})
-console.log(newFrutes);
+// frutes.forEach((frute) => {
+//     newFrutes += frute + ' ';
+// })
+// for(const frute of frutes.values()){
+//     newFrutes += frute + ' ';
+// }
+
+// console.log(newFrutes);
