2894354fb9f709384ac3c74cb74f64f87d15887b
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2023-10-14 09:06:34 +0000

    a reduce method added

--- .devcontainer/basics/problem.js
+++ .devcontainer/basics/problem.js
@@ -20,7 +20,7 @@
 //     return newArray;
 // }
 // console.log(RemoveDuplicate(myArray));
-const myArray = [1, 1,13,13, 2, 3, 4, 4, 4, 5, 5, 5, 6,1,5,1, 6, 6, 7, 7, 7, 7, 7, 8, 9,,2, 9, 9, 9, 9,12,12,12];
+//const myArray = [1, 1,13,13, 2, 3, 4, 4, 4, 5, 5, 5, 6,1,5,1, 6, 6, 7, 7, 7, 7, 7, 8, 9,,2, 9, 9, 9, 9,12,12,12];
 
 // function RemoveDuplicate(arr) {
 //     const newArray = arr.filter((value, index) => {
@@ -29,4 +29,14 @@ const myArray = [1, 1,13,13, 2, 3, 4, 4, 4, 5, 5, 5, 6,1,5,1, 6, 6, 7, 7, 7, 7, 
 //     return newArray;
 // }
 
+// console.log(RemoveDuplicate(myArray));
+// function RemoveDuplicate(arr){
+//     const newarry = arr.reduce((acc,carentvalue)=>{
+//         if(acc.indexOf(carentvalue) === -1){
+//             acc.push(carentvalue)
+//         }
+//         return acc;
+//     },[])
+//     return newarry;
+// }
 // console.log(RemoveDuplicate(myArray));
