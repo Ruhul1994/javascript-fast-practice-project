aa1f78f34cfda7fa4cb4894567a1a9f85de32ae9
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2023-10-23 13:19:28 +0000

    update the function

--- .devcontainer/basics/problem.js
+++ .devcontainer/basics/problem.js
@@ -139,16 +139,18 @@
 // Write a function that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz".
 //  For numbers that are multiples of both three and five, print "FizzBuzz".
 
-// function FizzBuzz(number){
-//    if(number % 3 ==0 && number % 5 ==0){
-//       console.log('FizzBuzz');
-//    }else if(number % 3 == 0){
-//       console.log('Fizz');
-//    }else if (number % 5 ==0){
-//       console.log('Buzz');
-//    }else{
-//       console.log(number);
-//    }
-// }
+function FizzBuzz(){
+    for(let i=1; i<=100; i++ ){
+   if(i % 3 ==0 && i % 5 ==0){
+      console.log('FizzBuzz');
+   }else if(i % 3 == 0){
+      console.log('Fizz');
+   }else if (i % 5 ==0){
+      console.log('Buzz');
+   }else{
+      console.log(i);
+   }
+}
+}
 
-// FizzBuzz(15);
+FizzBuzz();
