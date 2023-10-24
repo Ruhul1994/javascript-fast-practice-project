651ffb9ddbc84c1e31c3a708d50722579ac7bee1
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2023-10-24 17:55:57 +0000

    a palindrome checker function creat

--- .devcontainer/basics/problem.js
+++ .devcontainer/basics/problem.js
@@ -139,18 +139,36 @@
 // Write a function that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz".
 //  For numbers that are multiples of both three and five, print "FizzBuzz".
 
-function FizzBuzz(){
-    for(let i=1; i<=100; i++ ){
-   if(i % 3 ==0 && i % 5 ==0){
-      console.log('FizzBuzz');
-   }else if(i % 3 == 0){
-      console.log('Fizz');
-   }else if (i % 5 ==0){
-      console.log('Buzz');
-   }else{
-      console.log(i);
+// function FizzBuzz(){
+//     for(let i=1; i<=100; i++ ){
+//    if(i % 3 ==0 && i % 5 ==0){
+//       console.log('FizzBuzz');
+//    }else if(i % 3 == 0){
+//       console.log('Fizz');
+//    }else if (i % 5 ==0){
+//       console.log('Buzz');
+//    }else{
+//       console.log(i);
+//    }
+// }
+// }
+
+// FizzBuzz();
+// Problem 1: Palindrome Checker
+// Write a function that takes a string as input and returns true if it's a palindrome (reads the same backward as forward), and false otherwise.
+
+function ispalindrome(inputString){
+const clinString = inputString.replace(/[^A-Za-z1-9@#$]/g,'').toLowerCase();
+let leftPointer = 0;
+let rightPointer = clinString.length -1;
+while(leftPointer < rightPointer){
+   if(clinString[leftPointer] !== clinString[rightPointer]){
+      return false
    }
+   leftPointer ++;
+   rightPointer --;
 }
+return true;
 }
 
-FizzBuzz();
+console.log(ispalindrome('racecar'));
