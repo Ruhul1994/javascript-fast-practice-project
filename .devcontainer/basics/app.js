a5074b685e31e033a31fee02af9be2b5316000c6
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-03-20 07:55:58 +0000

    some erro fixd

--- .devcontainer/basics/app.js
+++ .devcontainer/basics/app.js
@@ -1,12 +1,15 @@
+//wrong code.
 // import { createInterface } from 'readline';
 // const rl = createInterface({
-//     input: Process.stdin,
-//     output: Process.stdout,
+//     input: process.stdin,
+//     output: process.stdout,
 // });
-// rl = rl.question("please inter your name :" , (name)=>{
+// rl.question("please inter your name :" , (name)=>{
 //     console.log(`you inter the name ${name}`)
 //     rl.close();
 // })
+//wright code.
+// input the name Name:
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
