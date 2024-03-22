52617f76b2ff584543a3e6c17e894a19c5f3eb2c
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-03-22 16:40:27 +0000

    string

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,9 @@
+let text1 = "gooodmorningsir";
+let text1Lenth = text1.length;
+console.log(text1Lenth);
+let result = text1.padEnd(16);
+console.log(result);
+
+let text = "Hello";
+let paddedText = text.padStart(10); // Pad with spaces to make the total length 10
+console.log(paddedText); // Output: "     Hello"
