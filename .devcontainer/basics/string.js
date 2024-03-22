888782fb7247087db8769242d4f48a37daa9257e
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-03-22 18:48:52 +0000

    search practice

--- .devcontainer/basics/string.js
+++ .devcontainer/basics/string.js
@@ -1,9 +1,25 @@
-let text1 = "gooodmorningsir";
-let text1Lenth = text1.length;
-console.log(text1Lenth);
-let result = text1.padEnd(16);
-console.log(result);
+//Example of padEnd / and padStart method;
+let text1 = "goood morning sir";
+// let text1Lenth = text1.length;
+// console.log(text1Lenth);
+// let result = text1.padEnd(16);
+// console.log(result);
 
-let text = "Hello";
-let paddedText = text.padEnd(18,'.'); // Pad with spaces to make the total length 10
-console.log(paddedText); // Output: "     Hello"
+// let text = "Hello";
+// let paddedText = text.padEnd(18,'.'); // Pad with spaces to make the total length 10
+// console.log(paddedText); // Output: "     Hello"
+
+// let text1Split = text1.split("i",2);
+// console.log(text1Split)
+// console.log(text1Split.length);
+
+//Example of localeCompare method.
+// let b= 'ghi';
+// let d='def';
+// let copare = b.localeCompare(d);
+// console.log(copare);
+
+//Example of search method
+let someText = 'this is the main Plan in my life but it is not working';
+let searchResult = someText.search(/i/i);
+console.log(searchResult);
