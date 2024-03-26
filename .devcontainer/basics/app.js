c4412c104493489997dc3304f35f8c2f852a90d6
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-03-26 19:14:22 +0000

    this is asynchronous file read

--- .devcontainer/basics/app.js
+++ .devcontainer/basics/app.js
@@ -11,20 +11,31 @@
 //wright code.
 // input the name Name:
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question("Please enter your name: ", (name) => {
+//     console.log(`You entered the name ${name}`);
+//     rl.close();
+// });
+
+// rl.on("close", ()=>{
+//     console.log('the interface is close');
+//     process.exit(0);
+// });
+
+// example of asynchronous file read.
+
+const fs = require('fs');
+
+fs.readFile('.devcontainer/basics/start.txt','utf-8', (error1,data1)=>{
+    console.log(data1);
+    fs.readFile(`.devcontainer/basics/${data1}.txt`,'utf-8', (error1,data1)=>{
+        console.log(data1);
+    });
 });
-
-rl.question("Please enter your name: ", (name) => {
-    console.log(`You entered the name ${name}`);
-    rl.close();
-});
-
-rl.on("close", ()=>{
-    console.log('the interface is close');
-    process.exit(0);
-});
-
+console.log('hello ruhul...')
