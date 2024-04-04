b0de03fd20edff18741f20949a3d8e2d9602ceda
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-04-04 05:13:15 +0000

    practice the inharitence.

--- .devcontainer/basics/class.js
+++ .devcontainer/basics/class.js
@@ -12,28 +12,53 @@
 // // console.log(newVapa.coconut);
 // console.log(newVapa.makePitha(6,4));
 
-class AddMactin {
-    constructor(){
-        this.array = [];
+// class AddMactin {
+//     constructor(){
+//         this.array = [];
+//     }
+//     makeArray(number){
+//         this.array.push(number);
+//         return this.array
+//     }
+//     lookUp(){
+//        return this.array[this.array.length-1];
+//     }
+//     removeElement(){
+//        return this.array.pop()
+//     }
+// }
+
+// let numberArray = new AddMactin();
+// numberArray.makeArray(5);
+// numberArray.makeArray(8);
+// numberArray.makeArray(23);
+// numberArray.makeArray(29);
+  
+// console.log(numberArray.lookUp());
+// numberArray.removeElement();
+// console.log(numberArray);
+
+// inhritance of javascript.
+
+class MobilePhone {
+    constructor(name,model){
+        this.name = name;
+        this.model = model;
     }
-    makeArray(number){
-        this.array.push(number);
-        return this.array
-    }
-    lookUp(){
-       return this.array[this.array.length-1];
-    }
-    removeElement(){
-       return this.array.pop()
+    aiSupport(){
+        console.log(`the ${this.name} brand phone model number ${this.model} has ai support.`)
     }
 }
 
-let numberArray = new AddMactin();
-numberArray.makeArray(5);
-numberArray.makeArray(8);
-numberArray.makeArray(23);
-numberArray.makeArray(29);
-  
-console.log(numberArray.lookUp());
-numberArray.removeElement();
-console.log(numberArray)
+class myMobile extends MobilePhone{     // extend use for give data from inharits.
+    constructor(name, model, wronty){
+        super(name,model); // super use for inheritence.
+        this.wronty = wronty;
+    }
+    sendWronty(){
+        console.log(`the ${this.name} this model has provided ${this.wronty} year wronty and granty.`)
+    }
+}
+
+const infinix = new myMobile('infinix','19',2);
+console.log(infinix.sendWronty());
