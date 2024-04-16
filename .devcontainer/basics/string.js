71a67f3c0807216126954c00ae5bb15a9ef65b57
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-04-16 20:02:20 +0000

    the reverse string practice.

--- .devcontainer/basics/string.js
+++ .devcontainer/basics/string.js
@@ -52,8 +52,68 @@ let para2 = 'I am not codding practice now';
 // b = a
 // console.log("4",b);
 // Refferance type valu all thime change the root of momory alocate place.
-let a =[1,2,3]
-let b = a;
-console.log("1",b);
-a.push(8);
-console.log("2",b);
+// let a =[1,2,3]
+// let b = a;
+// console.log("1",b);
+// a.push(8);
+// console.log("2",b);
+// reverch string.
+let demoString = 'Ruhulkuddus';
+// function reverchString(str){
+// let reverchst ='';   
+// for(let i = str.length -1; i >=0 ; i--){
+//     reverchst += str[i];
+// }
+// return reverchst;
+// }
+// console.log( reverchString(demoString));
+
+    // function reverse(str) {
+    //     let reversed = ''
+        
+    //     for(let char of str){
+    //         reversed = char + reversed
+    // }
+        
+    // return reversed 
+    // }
+
+    // console.log( reverse(demoString));
+
+    // function revurch(str){
+    //     let strrev =''
+    //    for(let i =0; i < str.length; i++){
+    //     strrev =  str[i] + strrev ; // it will added the string last to fast.
+    //    }
+    //    return strrev;
+    // }
+
+    // console.log(revurch('demoString'));
+
+ //the easy way to problem solve.
+
+//  function reverseStr(str){
+//     return str.split('').reverse().join('');
+//  }
+
+//  console.log(reverseStr('shakil'));
+
+// solve the problem for of loops.
+// function reverseStr(str){
+//         let reverseStr =''
+//         for(let string of str){
+//             reverseStr = string + reverseStr;
+//         }
+//         return reverseStr;
+//       }
+    
+//      console.log(reverseStr('monika'));
+
+// reverse the  number;
+
+function reverseNumber(n){
+    let reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n)
+};
+
+console.log( reverseNumber(41));
