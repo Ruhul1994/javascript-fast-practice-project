8fb5c196b2945f390e451d37e51c7b1afc550056
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-04-17 06:18:32 +0000

    creat algorithm.js file

---
+++ .devcontainer/basics/algorithm.js
@@ -0,0 +1,60 @@
+//reverch string.
+let demoString = 'Ruhulkuddus';
+// function reverchString(str){
+// let reverchst ='';   
+// for(let i = str.length -1; i >=0 ; i--){
+//     reverchst += str[i];
+// }
+// return reverchst;
+// }
+// console.log( reverchString(demoString));
+
+    // function reverse(str) {
+    //     let reversed = ''
+        
+    //     for(let char of str){
+    //         reversed = char + reversed
+    // }
+        
+    // return reversed 
+    // }
+
+    // console.log( reverse(demoString));
+
+    // function revurch(str){
+    //     let strrev =''
+    //    for(let i =0; i < str.length; i++){
+    //     strrev =  str[i] + strrev ; // it will added the string last to fast.
+    //    }
+    //    return strrev;
+    // }
+
+    // console.log(revurch('demoString'));
+
+ //the easy way to problem solve.
+
+//  function reverseStr(str){
+//     return str.split('').reverse().join('');
+//  }
+
+//  console.log(reverseStr('shakil'));
+
+// solve the problem for of loops.
+// function reverseStr(str){
+//         let reverseStr =''
+//         for(let string of str){
+//             reverseStr = string + reverseStr;
+//         }
+//         return reverseStr;
+//       }
+    
+//      console.log(reverseStr('monika'));
+
+// reverse the  number;
+
+// function reverseNumber(n){
+//     let reversed = n.toString().split('').reverse().join('');
+//     return parseInt(reversed) * Math.sign(n)
+// };
+
+// console.log( reverseNumber(41));
