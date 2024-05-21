d058ba2ff7f0bc92114cacaa5eb1d17d0b743511
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-05-21 22:17:55 +0000

    condition operater practice now

---
+++ .devcontainer/basics/condition.js
@@ -0,0 +1,19 @@
+let spid = 40;
+
+// let rightSpid = spid == 30 ? "your spid is normal" : spid >30 ? "you are so fast": "you are slow now";
+
+// console.log(rightSpid);
+
+let msg;
+switch (spid) {
+    case 30 :
+        msg = 'you are normal';
+        break;
+    case 40: 
+    msg = 'you are too fast';
+    break;
+    default:
+        msg = 'you are too slow now';
+}
+
+console.log(msg);
