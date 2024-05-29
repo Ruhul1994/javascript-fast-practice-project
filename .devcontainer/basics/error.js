638993c8e078fb6d95c91b12acb542a5ed5a0bb3
DevRuhul <95877022+Ruhul1994@users.noreply.github.com>
2024-05-29 22:46:48 +0000

    updet error handeling

--- .devcontainer/basics/error.js
+++ .devcontainer/basics/error.js
@@ -1,11 +1,25 @@
-try{
-    console.log("Hello sir.");
-}
-catch {
-    for(let i=i ; i<3; i++){
-        console.log('mini');
+function fun(){
+    try{
+        console.log("Hello sir.");
     }
+    catch(err) {
+        for(let i=i ; i<3; i++){
+            console.log('mini');
+        }
+    }
+    finally{
+        console.log('thank you sir')
+    }    
+
 }
-finally{
-    console.log('thank you sir')
-}
+
+fun();
+
+
+// try {
+//     const x = 10;
+//     if(x===10) throw  error;
+// }
+// catch {
+//     console.log('error');
+// }
